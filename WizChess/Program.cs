67d50e8a8f67c3e56c721036c7605d6c1e4b7602
sufal <sufal@sufalroy1997gmail.com>
2021-01-21 20:34:30 +0530

    Preliminary project setup

---
+++ WizChess/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace WizChess
+{
+    class Program
+    {
+        static void Main()
+        {
+            Game game = new Game(1280, 720);
+            game.Run();
+            game.Dispose();
+        }
+    }
+}
---
+++ WizChess/src/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizChess
+{
+    public class Game : IDisposable
+    {
+        public Game(int width, int height) 
+        {
+            Init();
+        }
+
+        private void Init()
+        { 
+
+        }
+
+        public void Run() 
+        { 
+
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
